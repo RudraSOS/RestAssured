4f12fdf720d210230038652992ba34c1552e3f8c
shankar <shantiomshankarjob@gmail.com>
2021-03-26 22:33:29 +0530

    RestAssured-Self practice

---
+++ src/test/java/restassuredExample/RestDay1.java
@@ -0,0 +1,11 @@
+package restassuredExample;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
+
+public class RestDay1 {
+    public static void main(String args[]){
+        RestAssured rs=new RestAssured();
+        //Header head=new Header();
+    }
+}
